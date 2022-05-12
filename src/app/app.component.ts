d0a757b79b7867b7f80081890af405d833f503af
Vitor Nunes do Nascimento <stecks10@gmail.com>
2022-05-12 17:10:13 -0300

    Adequando o template gerado e mais data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+  title = "AluraPic";
+  description = "Leao";
+  ulr =
+    "https://images.pexels.com/photos/9557885/pexels-photo-9557885.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
 }
+2;
